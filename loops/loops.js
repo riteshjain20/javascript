db38d51465906c84b8555130c24e1f9f2afea4cc
Ritesh Jain <rj4380790@gmail.com>
2026-08-18 17:17:55 +0530

    Add loops.html and loops.js with initial loop examples

---
+++ loops/loops.js
@@ -0,0 +1,82 @@
+
+// for(let i=1;i<=20;i++){
+//   console.log(i);
+// }
+
+// -------------------
+
+// let i = 20;
+// while(i>=1){
+//   console.log(i);
+//   i--;
+// }
+
+// ----------------
+
+// let i = 1;
+// do{
+//   console.log(i);
+//   i++;
+// }while(i<=50)
+
+
+// --------------------
+
+// for(let i=2;i<=100;i+=2){
+//   console.log(i);
+// }
+
+// ------------------------
+
+// let i = 1;
+// while(i<=100){
+//   if(i%2==1){
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+// ---------------
+
+// let i = 5;
+// do{
+//     console.log(i);
+//   i+=5;
+// }while(i<=100)
+
+// --------------------
+
+// let sum = 0;
+// for(i=1;i<=100;i++){
+//   sum = sum+i;
+// }
+// console.log(`sum is ${sum}`);
+
+// --------------------
+
+// let sum = 0;
+// let i = 2;
+// while(i<=100){
+//   sum = sum+i;
+//   i+=2
+// }
+// console.log(`sum of even number is : ${sum}`);
+
+// -------------------
+
+// let count = 0;
+// let i = 1;
+// do{
+//   count++;
+//   i++;
+// }while(i<=100)
+//   console.log(`${count} numbers are between 1 and 100`);
+
+// -----------------
+
+// for(let i=1;i<=10;i++){
+//   let square = i*i;
+//   console.log(`Square of ${i} is ${square}`);
+// }
+
+// ------level 2------------
