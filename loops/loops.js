805a2f992d39edeb19206ecc045221efc1d322c6
Ritesh Jain <rj4380790@gmail.com>
2026-08-18 18:17:26 +0530

    Add additional loop examples and exercises to loops.js

--- loops/loops.js
+++ loops/loops.js
@@ -80,3 +80,225 @@
 // }
 
 // ------level 2------------
+
+// Q1. Print 1 to 20---------------
+
+// for(let i=1;i<=20;i++){
+//   console.log(i);
+// }
+
+
+// let i = 1;
+// while(i<=20){
+//   console.log(i);
+//   i++;
+// }
+
+
+// let i = 1;
+// do{
+//   console.log(i);
+//   i++;
+// }while(i<=20)
+
+// Q2. Print 20 to 1-----------------------
+
+// for(let i=20;i>=1;i--){
+//   console.log(i);
+// }
+
+
+// let i = 20;
+// while(i>=1){
+//   console.log(i);
+//   i--;
+// }
+
+
+// let i = 20;
+// do{
+//   console.log(i);
+//   i--;
+// }while(i>=1)
+
+// Q3. Print even numbers 1–50-------------------------
+
+// for(let i = 2; i<=50;i+=2){
+//   console.log(i);
+// }
+
+
+// let i = 2;
+// while(i<=50){
+//   console.log(i);
+//   i+=2;
+// }
+
+
+// let i = 2;
+// do{
+//   console.log(i);
+//   i+=2;
+// }while(i<=50)
+
+// Q4. Print odd numbers 1–50-----------------------
+
+// for(let i=1;i<=50;i++){
+//   if(i%2!==0){
+//     console.log(i);
+//   }
+// }
+
+
+// let i = 1;
+// while(i<=50){
+//   if(i%2!==0){
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+
+// let i = 1;
+// do{
+//   if(i%2!==0){
+//     console.log(i);
+//   }
+//   i++;
+// }while(i<=50)
+
+// Q5. Print multiples of 5-------------
+
+// for(let i=5;i<=100;i+=5){
+//   console.log(i);
+// }
+
+
+// let i = 5;
+// while(i<=100){
+//   console.log(i);
+//   i+=5;
+// }
+
+
+// let i = 5;
+// do{
+//   console.log(i);
+//   i+=5;
+// }while(i<=100)
+
+// Q6. Print numbers divisible by 3-----------
+
+// for(let i = 3;i<=100;i+=3){
+//   console.log(`${i} is divisible by 3`);
+// }
+
+
+// let i = 3;
+// while(i<=100){
+//   console.log(`${i} is divisible bye 3`);
+//   i+=3;
+// }
+
+
+// let i = 3;
+// do{
+//   console.log(`${i} id divisible by 3`);
+//   i+=3;
+// }while(i<=100)
+
+// Q7. Print squares----------------
+
+// for(let i =1;i<=10;i++){
+//   let square = i*i;
+//   console.log(`square of ${i} is ${square}`);
+// }
+
+// let i =1;
+// while(i<=10){
+//   let sqaure = i*i;
+//   console.log(`sqaure of ${i} is ${sqaure}`);
+//   i++;
+// }
+
+
+// let i = 1;
+// do{
+//   let square = i*i;
+//   console.log(`square of ${i} is ${square}`);
+//   i++;
+// }while(i<=10)
+
+// Q8. Print cubes---------------
+
+// for(let i =1;i<=10;i++){
+//   let cube = i*i*i;
+//   console.log(`cube of ${i} is ${cube}`);
+// }
+
+
+// let i = 1;
+// while(i<=10){
+//   let cube = i*i*i;
+//   console.log(`cube of ${i} is ${cube}`);
+//   i++;
+// }
+
+
+// let i = 1;
+// do{
+//   let cube = i*i*i;
+//   console.log(`cube of ${i} is ${cube}`);
+//   i++;
+// }while(i<=10)
+
+
+// Q9. Print a number 10 times-----------
+
+// let num = 1;
+// for(let i=1;i<=10;i++){
+//   console.log(num);
+// }
+
+
+// let num = 1;
+// let i =1;
+// while(i<=10){
+//   console.log(num);
+//   i++;
+// }
+
+
+// let num = 1;
+// let i = 1;
+// do{
+//   console.log(num);
+//   i++;
+// }while(i<=10)
+
+
+// Q10. Print a range---------------
+
+// let start = 1;
+// let end = 20;
+// for(let i=start; i<=end; i++){
+//   console.log(i);
+// }
+
+
+// let start = 1;
+// let end = 20;
+// let i = start;
+// while(i<=end){
+//   console.log(i);
+//   i++;
+// }
+
+
+// let start = 1;
+// let end = 25;
+// let i = start;
+// do{
+//   console.log(i);
+//   i++;
+// }while(i<=end)
